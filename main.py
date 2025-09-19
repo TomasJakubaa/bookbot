3a6e67c13a824fb6d32389538ba7ede71d7dd025
TomasJakubaa <tomasjakubaa@gmail.com>
2025-09-19 15:10:12 +0200

    making sys in main.py work

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import sys
 import stats
 
 def main():
 
-	path_to_file = "books/frankenstein.txt"
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	path_to_file = sys.argv[1]
 	book_text = stats.get_book_text(path_to_file)
 	words_counted = stats.count_words(book_text)
 	characters_counted = stats.count_characters(book_text)
